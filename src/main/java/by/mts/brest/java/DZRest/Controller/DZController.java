f313b1f2fbe8a9cff24074be2f58ff0675451fb9
orn974@gmail.com <Grigorik>
2021-05-04 14:22:11 +0300

    dzRest+Lombok

--- src/main/java/by/mts/brest/java/DZRest/Controller/DZController.java
+++ src/main/java/by/mts/brest/java/DZRest/Controller/DZController.java
@@ -1,21 +1,17 @@
 package by.mts.brest.java.DZRest.Controller;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-
 @Controller
 
 public class DZController {
 
     RestTemplate restTemplate;
-    final String ROOT_URL_DZ = "http://192.168.3.220:8080/";
+    final String ROOT_URL_DZ = "http://192.168.3.222:8080/";
     @GetMapping("/dz")
       public String dzGet (Model model){
         restTemplate=new RestTemplate();
--- src/main/java/by/mts/brest/java/DZRest/Controller/Employee.java
+++ src/main/java/by/mts/brest/java/DZRest/Controller/Employee.java
@@ -1,5 +1,14 @@
 package by.mts.brest.java.DZRest.Controller;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
     static int portal_id = 0;
     private String name;
@@ -9,91 +18,14 @@ public class Employee {
     private String position;
     private String adress_work;
     private Integer age;
-    // private String grade;
     private String telephone_stack;
     private boolean at_work = true;
-
-    public static int getPortal_id() {
-        return portal_id;
-    }
-
-    public static void setPortal_id(int portal_id) {
-        Employee.portal_id = portal_id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getPatronymic() {
-        return patronymic;
-    }
-
-    public void setPatronymic(String patronymic) {
-        this.patronymic = patronymic;
-    }
-
-    public String getDatebirth() {
-        return datebirth;
-    }
-
-    public void setDatebirth(String datebirth) {
-        this.datebirth = datebirth;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public String getAdress_work() {
-        return adress_work;
-    }
-
-    public void setAdress_work(String adress_work) {
-        this.adress_work = adress_work;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getTelephone_stack() {
-        return telephone_stack;
-    }
-
-    public void setTelephone_stack(String telephone_stack) {
-        this.telephone_stack = telephone_stack;
-    }
 
     public Employee(String surname, String name, Integer age, String position) {
         this.name = name;
         this.surname = surname;
         this.position = position;
         this.age = age;
-    }
-
-    public Employee() {
-        System.out.println();
     }
 
     public Employee(String name, String surname, String patronymic, String datebirth, String position, String adress_work, String telephone_stack, boolean at_work/*, String grade*/) {
@@ -105,7 +37,5 @@ public class Employee {
         this.adress_work = adress_work;
         this.telephone_stack = telephone_stack;
         this.at_work = at_work;
-        //this.grade = grade;
     }
-
 }
